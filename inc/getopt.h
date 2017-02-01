1dec09a18174cfc31285b87a6e4d12e2debf29b4
Dan Fields <dan.fields@nexenta.com>
2017-02-01 06:36:52 -0500

    Initial version from archive

---
+++ inc/getopt.h
@@ -0,0 +1,19 @@
+#if !EFI
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#endif
+
+#ifndef _GETOPT_
+#define _GETOPT_
+
+int getopt(int argc, char *argv[], char *optstring);
+
+extern char *optarg;		/* returned arg to go with this option */
+extern int optind;		/* index to next argv element to process */
+extern int opterr;		/* should error messages be printed? */
+extern int optopt;
+
+#define BADCH ('?')
+
+#endif /* _GETOPT */
